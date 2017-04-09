17337e6e4bdda5bfad681f3655d3256230ded0d3
luick klippel <luick klippel>
2017-04-09 15:39:06 +0200

    applied new sensors

---
+++ CAR_SERVER/src/de/ye/car_SENSOR_FRONT/CONFIG.java
@@ -0,0 +1,23 @@
+package de.ye.car_SENSOR_FRONT;
+
+public class CONFIG {
+	
+	//Latency between each pass
+	public static int LATENCY=200;
+
+	//Reaction limits -> SLOW
+	public static int S_R_DIST=80;
+	
+
+	//Reaction limits -> FAST
+	public static int F_R_DIST=90;
+	
+	//Reaction limits -> ULTRA_FAST
+	public static int UF_R_DIST=150;
+	
+	public static int LIFT_SIDE=90;
+	
+	public static int LATENCY_SIDE=50;
+	
+	
+}
---
+++ CAR_SERVER/src/de/ye/car_SENSOR_FRONT/SENS_FRONT_MIDDLE_LEFT.java
@@ -0,0 +1,150 @@
+package de.ye.car_SENSOR_FRONT;
+
+import com.pi4j.io.gpio.GpioController;
+import com.pi4j.io.gpio.GpioFactory;
+import com.pi4j.io.gpio.GpioPinDigitalInput;
+import com.pi4j.io.gpio.GpioPinDigitalOutput;
+import com.pi4j.io.gpio.Pin;
+import com.pi4j.io.gpio.RaspiPin;
+
+import de.ye.car_eng.ENGINE;
+import de.ye.car_modi.THREAD_MODI;
+
+public class SENS_FRONT_MIDDLE_LEFT implements Runnable{
+
+    private final static float SOUND_SPEED = 340.29f;  
+    
+    private final static int TRIG_DURATION_IN_MICROS = 10;
+    private final static int WAIT_DURATION_IN_MILLIS = 60; 
+
+    private final static int TIMEOUT = 2100;
+    private static int dist;
+    
+    private final static GpioController gpio = GpioFactory.getInstance();
+    
+    private GpioPinDigitalInput echoPin;
+    private GpioPinDigitalOutput trigPin;
+    public static int sens;
+            
+    private void prep_s( Pin echoPin, Pin trigPin ) {
+        this.echoPin = gpio.provisionDigitalInputPin( echoPin );
+        this.trigPin = gpio.provisionDigitalOutputPin( trigPin );
+        this.trigPin.low();
+    }
+    
+    public float measureDistance() throws TimeoutException {
+        this.triggerSensor();
+        this.waitForSignal();
+        long duration = this.measureSignal();
+        
+        return duration * SOUND_SPEED / ( 2 * 10000 );
+    }
+
+    private void triggerSensor() {
+        try {
+            this.trigPin.high();
+            Thread.sleep( 0, TRIG_DURATION_IN_MICROS * 1000 );
+            this.trigPin.low();
+        } catch (InterruptedException ex) {
+        	//dist=5;
+            System.err.println( "" );
+        }
+    }
+    
+    private void waitForSignal() throws TimeoutException {
+        int countdown = TIMEOUT;
+        
+        while( this.echoPin.isLow() && countdown > 0 ) {
+            countdown--;
+        }
+        
+        if( countdown <= 0 ) {
+        	//dist=5;
+            throw new TimeoutException( "" );
+        }
+    }
+    
+    private long measureSignal() throws TimeoutException {
+        int countdown = TIMEOUT;
+        long start = System.nanoTime();
+        while( this.echoPin.isHigh() && countdown > 0 ) {
+            countdown--;
+        }
+        long end = System.nanoTime();
+        
+        if( countdown <= 0 ) {
+        	//dist=5;
+            throw new TimeoutException("");
+        }
+        
+        return (long)Math.ceil( ( end - start ) / 1000.0 );  
+    }
+    
+    public void run() {
+    	////////
+        Pin echoPin = RaspiPin.GPIO_27; 
+        Pin trigPin = RaspiPin.GPIO_26; 
+    	////////
+        prep_s( echoPin, trigPin );
+        
+        while( true ) {
+            try {
+            	Thread.sleep(CONFIG.LATENCY);
+            	dist = Math.round(this.measureDistance());
+            	//System.out.println("MIDDLE_LEFT: "+dist);
+                if(THREAD_SENS.I_S_FRONT==true){
+                	if(THREAD_MODI.MODI.equalsIgnoreCase("s")){
+                    	if(dist <=CONFIG.S_R_DIST){
+                    		//System.out.println("MOTOR-OFF");
+                        	ENGINE.STOP();
+                    	} else {
+                    		ENGINE.START();
+                    	}
+                	}
+                	if(THREAD_MODI.MODI.equalsIgnoreCase("f")){
+                    	if(dist <=CONFIG.F_R_DIST){
+                    		//System.out.println("MOTOR-OFF");
+                        	ENGINE.STOP();
+                    	} else {
+                    		ENGINE.START();
+                    	}
+                	}
+
+                	if(THREAD_MODI.MODI.equalsIgnoreCase("uf")){
+                    	if(dist <=CONFIG.UF_R_DIST){
+                    		//System.out.println("MOTOR-OFF");
+                        	ENGINE.STOP();
+                    	} else {
+                    		ENGINE.START();
+                    	}
+                	}
+                }
+            }
+            catch( TimeoutException e ) {
+                System.err.println( e );
+            } catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+            try {
+                Thread.sleep( WAIT_DURATION_IN_MILLIS );
+            } catch (InterruptedException ex) {
+                System.err.println( "Interrupt during trigger" );
+            }
+        }
+    }
+    private static class TimeoutException extends Exception {
+
+        private final String reason;
+        
+        public TimeoutException( String reason ) {
+            this.reason = reason;
+        }
+        
+        @Override
+        public String toString() {
+            return this.reason;
+        }
+    }
+}
